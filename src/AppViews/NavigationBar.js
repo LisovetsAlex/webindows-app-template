b0ec45b69172b467797adb4b0bd1a4a835fa1cc3
BigManRoss <bigmanrosssteam@gmail.com>
2024-04-16 16:36:39 +0200

    feat: added dropdown

--- src/AppViews/NavigationBar.js
+++ src/AppViews/NavigationBar.js
@@ -1,12 +1,52 @@
 import { ButtonBarContainer, ButtonBar } from "@webindows/ButtonBars";
 import Button from "@webindows/components/common/Button";
+import DropdownItem from "@webindows/components/common/Dropdown/DropdownItem";
+import createDropdown from "@webindows/components/common/Dropdown";
 
 export default function NavigationBar() {
     return (
         <ButtonBarContainer>
             <ButtonBar>
-                <Button text="File" disabled={false} />
-                <Button text="Edit" disabled={false} />
+                <Button
+                    text="File"
+                    disabled={false}
+                    onClick={() => {
+                        createDropdown("App_FileBtn", "App_FileDD", [
+                            <DropdownItem
+                                text="New File"
+                                onClick={() => {
+                                    console.log("New File");
+                                }}
+                            />,
+                            <DropdownItem
+                                text="Open File"
+                                onClick={() => {
+                                    console.log("Open File");
+                                }}
+                            />,
+                        ]);
+                    }}
+                />
+                <Button
+                    text="Edit"
+                    disabled={false}
+                    onClick={() => {
+                        createDropdown("App_EditBtn", "App_EditDD", [
+                            <DropdownItem
+                                text="Undo"
+                                onClick={() => {
+                                    console.log("Undo");
+                                }}
+                            />,
+                            <DropdownItem
+                                text="Redo"
+                                onClick={() => {
+                                    console.log("Redo");
+                                }}
+                            />,
+                        ]);
+                    }}
+                />
                 <Button text="View" disabled={true} />
                 <Button text="Extra" disabled={true} />
                 <Button text="?" disabled={true} />
